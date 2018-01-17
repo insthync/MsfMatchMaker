159152442b204e046e1ee03b1606f12e2d961747
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-01-17 16:56:10 +0700

    Force developer to use UIMobaLobby for uiLobby

--- Scripts/Integrations/Moba/MobaMatchMakerClient.cs
+++ Scripts/Integrations/Moba/MobaMatchMakerClient.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 using Barebones.MasterServer;
 using Barebones.Networking;
 
@@ -13,7 +14,22 @@ public class MobaMatchMakerClient : QueueMatchMakerClient
         base.Awake();
 
         Msf.Client.SetHandler((short)MobaMatchMakerOpCodes.AllPlayersReady, OnAllPlayersReady);
+
+        if (uiLobby == null)
+            uiLobby = FindObjectOfType<UIMobaLobby>();
     }
+
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        if (uiLobby != null && !(uiLobby is UIMobaLobby))
+        {
+            Debug.LogWarning("`uiLobby` for `MobaMatchMakerClient` must be `UIMobaLobby`");
+            uiLobby = null;
+            EditorUtility.SetDirty(this);
+        }
+    }
+#endif
 
     protected virtual void OnAllPlayersReady(IIncommingMessage message)
     {
