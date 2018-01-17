617baea0b2c53496493cba8d8dd0ad1ffdc6e742
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-01-17 14:23:04 +0700

    Add ability for MOBA clients to know when all players ready

---
+++ Scripts/Integrations/Moba/MobaMatchMakerClient.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Barebones.MasterServer;
+using Barebones.Networking;
+
+public class MobaMatchMakerClient : QueueMatchMakerClient
+{
+    public event System.Action AllPlayersReady;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        Msf.Client.SetHandler((short)MobaMatchMakerOpCodes.AllPlayersReady, OnAllPlayersReady);
+    }
+
+    protected virtual void OnAllPlayersReady(IIncommingMessage message)
+    {
+        if (AllPlayersReady != null)
+            AllPlayersReady.Invoke();
+    }
+}
---
+++ Scripts/Integrations/Moba/MobaMatchMakerOpCodes.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MobaMatchMakerOpCodes : short
+{
+    AllPlayersReady = 20001,
+}
