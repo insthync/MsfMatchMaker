65a2ad4a9ade5860606a352944856ac10f6ac891
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-01-18 15:58:37 +0700

    Add moba lobby UIs

---
+++ Scripts/Integrations/Moba/UIs/BaseUIMobaLobby.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Barebones.MasterServer;
+
+public class BaseUIMobaLobby : MonoBehaviour
+{
+    public UIMobaLobbyUser userPrefab;
+    public Text gameStatus;
+    public Button readyButton;
+    public Transform alliesContainer;
+    public Transform enemiesContainer;
+    
+    protected readonly Dictionary<string, LobbyUserUi> users = new Dictionary<string, LobbyUserUi>();
+    public string CurrentUser { get; protected set; }
+    public string CurrentTeam { get; protected set; }
+    public JoinedLobby JoinedLobby { get; protected set; }
+    
+    public virtual void Initialize(JoinedLobby lobby)
+    {
+        JoinedLobby = lobby;
+        CurrentUser = lobby.Data.CurrentUserUsername;
+        CurrentTeam = lobby.Members[CurrentUser].Team;
+
+        for (var i = alliesContainer.childCount - 1; i >= 0; --i)
+            Destroy(alliesContainer.GetChild(i).gameObject);
+
+        for (var i = enemiesContainer.childCount - 1; i >= 0; --i)
+            Destroy(enemiesContainer.GetChild(i).gameObject);
+
+        users.Clear();
+
+        foreach (var player in lobby.Members)
+            users.Add(player.Key, CreateMemberView(player.Value));
+
+        UpdateReadyButton();
+    }
+
+    protected virtual LobbyUserUi CreateMemberView(LobbyMemberData data)
+    {
+        // Get user view
+        var user = Instantiate(userPrefab);
+        user.Reset();
+        user.gameObject.SetActive(true);
+        user.Setup(data);
+        user.IsCurrentPlayer = data.Username == CurrentUser;
+
+        // Add user to team
+        var teamContainer = data.Team.Equals(CurrentTeam) ? alliesContainer : enemiesContainer;
+        user.transform.SetParent(teamContainer, false);
+        user.transform.SetAsLastSibling();
+
+        user.SetReadyStatusVisibility(AreUserReadyStatesVisible());
+
+        // Generate username text
+        user.Username.text = data.Username;
+
+        return user;
+    }
+
+    /// <summary>
+    /// Returns true if user ready states should be visible
+    /// </summary>
+    /// <returns></returns>
+    protected bool AreUserReadyStatesVisible()
+    {
+        return JoinedLobby.State == LobbyState.Preparations && JoinedLobby.Data.EnableReadySystem;
+    }
+
+    public virtual void OnMemberJoined(LobbyMemberData member)
+    {
+        users.Add(member.Username, CreateMemberView(member));
+    }
+
+    public virtual void OnMemberLeft(LobbyMemberData member)
+    {
+        LobbyUserUi user;
+        if (!users.TryGetValue(member.Username, out user))
+            return;
+
+        Destroy(user.gameObject);
+        users.Remove(member.Username);
+    }
+
+
+    public virtual void OnMemberReadyStatusChanged(LobbyMemberData member, bool isReady)
+    {
+        LobbyUserUi user;
+        if (!users.TryGetValue(member.Username, out user))
+            return;
+
+        user.SetReady(isReady);
+
+        UpdateReadyButton();
+    }
+
+    public virtual void OnMemberTeamChanged(LobbyMemberData member, LobbyTeamData team)
+    {
+        LobbyUserUi user;
+        if (!users.TryGetValue(member.Username, out user))
+            return;
+
+        // Player changed teams
+        var teamContainer = team.Name.Equals(CurrentTeam) ? alliesContainer : enemiesContainer;
+        user.transform.SetParent(teamContainer, false);
+
+        UpdateReadyButton();
+    }
+
+    public virtual void OnLobbyLeft()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public virtual void OnLobbyStatusTextChanged(string statusText)
+    {
+        gameStatus.text = statusText;
+    }
+
+    protected virtual void UpdateReadyButton()
+    {
+        var user = users[CurrentUser];
+
+        if (readyButton != null)
+            readyButton.enabled = !JoinedLobby.Members[CurrentUser].IsReady;
+    }
+
+    public virtual void OnClickReady()
+    {
+        // Can ready only once
+        if (!JoinedLobby.Members[CurrentUser].IsReady)
+            JoinedLobby.SetReadyStatus(true);
+
+        UpdateReadyButton();
+    }
+}
---
+++ Scripts/Integrations/Moba/UIs/UIMobaCharacterSelectionLobby.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Barebones.MasterServer;
+using Barebones.Utils;
+using System;
+
+public class UIMobaCharacterSelectionLobby : BaseUIMobaLobby
+{
+    public UIMobaLobbyChat uiChat;
+
+    public override void Initialize(JoinedLobby lobby)
+    {
+        base.Initialize(lobby);
+
+        if (uiChat != null)
+        {
+            uiChat.Clear();
+        }
+    }
+
+    public virtual void OnChatMessageReceived(LobbyChatPacket packet)
+    {
+        LobbyMemberData member;
+        if (JoinedLobby.Members.TryGetValue(packet.Sender, out member) && member.Team.Equals(CurrentTeam))
+            uiChat.OnMessageReceived(packet);
+    }
+
+    public virtual void OnCharacterChanged(string username, string value)
+    {
+
+    }
+}
