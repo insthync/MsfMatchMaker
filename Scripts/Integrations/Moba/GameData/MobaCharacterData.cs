9789afff1915e87b74604ae8f1026120de186508
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-01-18 16:05:12 +0700

    Character data for selection WIP

---
+++ Scripts/Integrations/Moba/GameData/MobaCharacterData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobaCharacterData : ScriptableObject
+{
+    public string title;
+    public string description;
+    public Sprite icon;
+}
