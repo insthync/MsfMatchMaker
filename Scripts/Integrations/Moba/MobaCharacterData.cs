75e741b99a98e34de52bb7a3c5ac574c1b34e6a5
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-01-18 16:36:32 +0700

    Add menu for moba character data

--- Scripts/Integrations/Moba/MobaCharacterData.cs
+++ Scripts/Integrations/Moba/MobaCharacterData.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "MobaCharacterData", menuName = "MobaGameData/MobaCharacterData")]
 public class MobaCharacterData : ScriptableObject
 {
     public string title;
